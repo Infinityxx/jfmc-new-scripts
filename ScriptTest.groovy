7fd5baf5f6e0e02d56fdd695a0f6620d56a4d125
jfrogmc <jfrogmc@instance-3.c.support-prod-157422.internal>
2018-08-16 11:48:06 +0000

    test

---
+++ ScriptTest.groovy
@@ -0,0 +1,13 @@
+artKey = userInput(
+  name : "artif",
+  type : "ARTIFACTORY",
+  value : "",
+  description:"please provide a value"
+  )
+
+team = userInput (
+  name :"team",
+  type: "STRING",
+  value: "",
+  description:"please provide a value"
+  )
